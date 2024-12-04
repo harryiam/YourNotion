d8b9cc25c9338e6dc40bc8de3a8a36974d025142
harryiam <ambati.hariprasad7@gmail.com>
2024-12-04 20:15:10 +0530

    created a react app

---
+++ yournotionproject/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hitesh's and harry's Notion website</h1>
+    </div>
+  );
+}
+
+export default App;
